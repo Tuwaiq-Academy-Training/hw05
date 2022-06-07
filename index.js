455232beec65e066cd9a7ea76c888c73b82725c3
Essa433 <60841988+Essa433@users.noreply.github.com>
2022-06-08 02:55:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// export default Array = [
-//   "Iphone,4300,1,an apple mobile phone ,15%,Mobile",
-//   "Galaxy S22,3200,1,an Android Mobile Phone,15%,Mobile",
-//   "USB Type-C Charger,50,2,a type-c charger,5%,Utils",
-//   "Cappuccino,12,4,a caffine drink,0%,Drinks",
-//   "Chair,100,2,A desk Chair,20%,Furniture",
-//   "Burger Meal,40,3,a Macdonald Meal,40%,Food",
-// ];
-
-
-export let array = [
+let array = [
   {
     itemName: "Iphone",
     price: 4300,
@@ -58,3 +48,15 @@ export let array = [
     type: "Food",
   },
 ]
+
+  let newarrProducts = array.map(
+    item =>
+      (newObj = {
+        product_name: item.itemName,
+        total_price: item.price * item.quantity * (1 - item.tax_percent),
+        type: item.type,
+        compound_name: item.itemName + "-" + item.price * item.quantity * (1 - item.tax_percent),
+      })
+  )
+  
+  console.log(newarrProducts)
